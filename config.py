7817c072621ce8a46ff1481c5f8386040b092fd9
kafiirgie <kafiirgie20@gmail.com>
2022-12-15 23:15:30 +0700

    update config

--- config.py
+++ config.py
@@ -10,9 +10,4 @@ app.config['MYSQL_DATABASE_PASSWORD'] = '7601labtek5'
 app.config['MYSQL_DATABASE_DB'] = 'wakacipu_livingcost'
 app.config['MYSQL_DATABASE_HOST'] = '103.163.138.244'
 
-# app.config['MYSQL_DATABASE_USER'] = 'wipeeebm_rakha'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'Miscrit10'
-# app.config['MYSQL_DATABASE_DB'] = 'wipeeebm_Google_Data'
-# app.config['MYSQL_DATABASE_HOST'] = '103.163.138.244'
-
 mysql.init_app(app)
